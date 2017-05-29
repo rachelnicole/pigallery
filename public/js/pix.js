1cf75d840a114302057358ee8795e913e0144f52
rachelnicole <loveless@gmail.com>
2017-05-29 15:05:20 -0400

    broken url

--- public/js/pix.js
+++ public/js/pix.js
@@ -4,7 +4,7 @@ function pickArt(altTag) {
   artPiece = altTag;
   console.log('art picked' + artPiece);
   socket.emit('artPiece', artPiece);
-  $('.gallery').append('<img src=&quot;public/images/' + artPiece + '-lrg.gif&quot;>');
+  $('.gallery').append('<img src="public/images/' + artPiece + '-lrg.gif">');
   setTimeout(function() {
     $('.gallery').empty();
   }, 2000)
