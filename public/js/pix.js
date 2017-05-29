23d79b77b2f7729561124f1573c5973ff409291a
rachelnicole <loveless@gmail.com>
2017-05-29 12:06:11 -0400

    changing gallery interactions

--- public/js/pix.js
+++ public/js/pix.js
@@ -1,7 +1,14 @@
 var socket = io();
 
-function pickArt() {
-  artNumber = $('input[name=art]:checked').val();
+function pickArt(altTag) {
+  artNumber = altTag;
   console.log('art picked' + artNumber);
   socket.emit('artNumber', artNumber);
 };
+
+$('.galleryItem').click(function(e){
+    e.preventDefault();
+    var alt = $(this).children("img").attr("alt");
+
+    pickArt(alt);
+});
